a309a76ee1a1fabe26044872bc0a2ff43efb24b7
skulkarni0102 <97902152+skulkarni0102@users.noreply.github.com>
2023-07-26 17:40:00 +0530

    Update settings.py

--- backend/weather_backend/settings.py
+++ backend/weather_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-jbp2t(09s_ps%jxh6@evj*5%&%z4kb191us&qncpz38-(y7ddj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
